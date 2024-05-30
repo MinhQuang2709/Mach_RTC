832018bdf970a1494af4382253fe68c1ab28b95e
LAPTOP-0F1E6EBH\LENOVO <minhq3102@gmail.com>
2024-05-30 17:28:49 +0700

    tách giờ phút giây

--- Mach_RTC/sketch_may29a/sketch_may29a.ino
+++ Mach_RTC/sketch_may29a/sketch_may29a.ino
@@ -9,32 +9,59 @@ void loop()
   while(Serial.available() > 0)
   {
     static char hoursInput[8];
+    static char minutesInput[8];
+    static char secondsInput[8];
     static char message[128];
     static unsigned int dem = 0;
+    static int cntMinute = 0;
+    static int cntSecond = 0;
     char inByte = Serial.read();
 
     if(inByte != '\n')
     {
-      message[dem] = inByte;     
-      hoursInput[dem] = inByte;
+      message[dem] = inByte;   
+      if(dem <= 2)
+      {
+        hoursInput[dem] = inByte;
+      }
+
+      if (dem > 2 && dem < 5)
+      {
+        minutesInput[cntMinute] = inByte;
+        cntMinute++;
+      }
+
+      if (dem > 5 && dem <=7)
+      {
+        secondsInput[cntSecond] = inByte;
+        cntSecond++;
+      }
       dem++;
     }
 
     else
     {
+      hoursInput[2] = '\0';
+      minutesInput[2] = '\0';
+      secondsInput[2] = '\0';
       message[dem] = '\0';
       Serial.println(message);
-      for(int i=0; i<2; i++){
-        Serial.print(hoursInput[i]);
-      }
-      Serial.println();
-      // Serial.println(hoursInput);
-      // int number = atoi(message);
-      // int val = number+10;
-      // Serial.println(val);
+      // for(int i=0; i<2; i++){
+      //   Serial.print(hoursInput[i]);
+      // }
+      // Serial.println();
+
+      int hours = atoi(hoursInput);
+      int minutes = atoi(minutesInput);
+      int seconds = atoi(secondsInput);
+      Serial.println(hours);
+      Serial.println(minutes);
+      Serial.println(seconds);
       dem = 0;
+      cntMinute = 0;
+      cntSecond = 0;
     }
   }
 }
 
-// 12  25 24
+//12 25 24
